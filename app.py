6b34d4064dd2b042fbbc7edacdbcde586ce88fcf
Thierry <115188454+Calonnec@users.noreply.github.com>
2023-03-18 15:32:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ if run_code:
     #-------------Model stuffs--------------------------------
 
     crop_path = sorted(glob.glob(save_path + "*.jpg"))
+    print(crop_path)
 
     crop_imgs = select_image(crop_path)
 
